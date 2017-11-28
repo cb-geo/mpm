adc7bb96dbe3328216370205fee70a33fdb2aab0
Krishna Kumar <kks32@cam.ac.uk>
2017-11-28 18:39:16 +0000

    :dart: Assign cell pointer to particle 3D

--- tests/particle_test.cc
+++ tests/particle_test.cc
@@ -153,7 +153,14 @@ TEST_CASE("Particle is checked for 2D case", "[particle][2D]") {
 
 //! \brief Check particle class for 3D case
 TEST_CASE("Particle is checked for 3D case", "[particle][3D]") {
+  // Dimension
   const unsigned Dim = 3;
+  // Number of nodes per cell
+  const unsigned Nnodes = 8;
+  // Tolerance
+  const double Tolerance = 1.E-7;
+
+  // Coordinates
   Eigen::Vector3d coords;
   coords.setZero();
 
@@ -174,8 +181,7 @@ TEST_CASE("Particle is checked for 3D case", "[particle][3D]") {
   //! Test coordinates function
   SECTION("coordinates function is checked") {
     mpm::Index id = 0;
-    const double Tolerance = 1.E-7;
-
+    // Create particle
     auto particle = std::make_shared<mpm::Particle<Dim>>(id, coords);
 
     //! Check for coordinates being zero
@@ -203,5 +209,53 @@ TEST_CASE("Particle is checked for 3D case", "[particle][3D]") {
       REQUIRE(coordinates(i) == Approx(coords(i)).epsilon(Tolerance));
 
     REQUIRE(coordinates.size() == Dim);
+  }
+
+  //! Test assign cell pointer to particle
+  SECTION("Add a pointer to a cell to particle") {
+    // Add particle
+    mpm::Index id = 0;
+    auto particle = std::make_shared<mpm::Particle<Dim>>(id, coords);
+    // Create cell
+    auto cell = std::make_shared<mpm::Cell<Dim>>(0, Nnodes);
+    // Add nodes
+    coords << 0, 0, 0;
+    auto node0 = std::make_shared<mpm::Node<Dim>>(0, coords);
+
+    coords << 1, 0, 0;
+    auto node1 = std::make_shared<mpm::Node<Dim>>(1, coords);
+
+    coords << 0, 1, 0;
+    auto node2 = std::make_shared<mpm::Node<Dim>>(2, coords);
+
+    coords << 1, 1, 0;
+    auto node3 = std::make_shared<mpm::Node<Dim>>(3, coords);
+
+    coords << 0, 0, 1;
+    auto node4 = std::make_shared<mpm::Node<Dim>>(4, coords);
+
+    coords << 1, 0, 1;
+    auto node5 = std::make_shared<mpm::Node<Dim>>(5, coords);
+
+    coords << 0, 1, 1;
+    auto node6 = std::make_shared<mpm::Node<Dim>>(6, coords);
+
+    coords << 1, 1, 1;
+    auto node7 = std::make_shared<mpm::Node<Dim>>(7, coords);
+
+    cell->add_node(0, node0);
+    cell->add_node(1, node1);
+    cell->add_node(2, node2);
+    cell->add_node(3, node3);
+    cell->add_node(4, node4);
+    cell->add_node(5, node5);
+    cell->add_node(6, node6);
+    cell->add_node(7, node7);
+    REQUIRE(cell->nnodes() == 8);
+
+    // Add cell to particle
+    REQUIRE(cell->status() == false);
+    particle->assign_cell(cell);
+    REQUIRE(cell->status() == true);
   }
 }
