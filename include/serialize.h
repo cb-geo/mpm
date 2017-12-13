c5434a3608087ae0767c5fb1c113d73edeb8e35e
Krishna Kumar <kks32@cam.ac.uk>
2017-12-13 11:55:41 +0000

    :fire: Remove unused includes

--- include/serialize.h
+++ include/serialize.h
@@ -1,12 +1,6 @@
 #ifndef MPM_SERIALIZE_H_
 #define MPM_SERIALIZE_H_
 
-#include <array>
-#include <iostream>
-#include <limits>
-#include <memory>
-#include <vector>
-
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/serialization/array.hpp>
