3fd4dd509f3ac45bea119bb9e9bc4cd9a264ecd9
Shyamini Ekanayake <shyaminiekanayake@Shyaminis-MacBook-Pro.local>
2018-02-02 18:25:13 +0000

    created a rough framework for the algorithm

--- include/explicitonephase_mpm.h
+++ include/explicitonephase_mpm.h
@@ -2,8 +2,9 @@
 #define MPM_PARTICLE_H_
 
 #include "mpm.h"
-#include "cell.h"
+#include "container.h"
 #include "particle.h"
+
 
 namespace mpm {
 
@@ -12,8 +13,30 @@ namespace mpm {
 //! \details ExplicitOnePhaseMpm class: dt
 //! \tparam Tdim Dimension
 template <unsigned Tdim>
-class ExplicitOnePhaseMpm : public Mpm<Tdim> {
+class ExplicitOnePhaseMpm : public MPM<Tdim> {
+ public:
+  //! Default constructor
+  ExplicitOnePhaseMpm() {};
+  
 
+  // container 1: container of particles
+  // container 2: container of cells or nodes?
+
+  // map particle mass to nodes
+  // map particle momentum to nodes
+  // compute nodal velocity
+
+  // body force
+  // traction force
+  // internal force
+
+  // solve equation of motion
+
+  // update particles (stress , velocity, position)
+
+ private:
+  mpm::Container<mpm::ParticleBase<Tdim>> particles_;
+  
 }; // ExplicitOnePhaseMpm class
 } // namespace mpm
 
