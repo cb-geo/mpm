32288b95cfd994c32e01e053079f5ffe5de972b1
Krishna Kumar <kks32@cam.ac.uk>
2018-03-14 18:15:37 +0000

    :fire: Remove unused comments in MPM explicit

--- include/mpm_explicit.h
+++ include/mpm_explicit.h
@@ -17,18 +17,6 @@ class MPMExplicit : public MPM<Tdim> {
   //! Default constructor
   MPMExplicit() = default;
 
-  // map particle mass to nodes
-  // map particle momentum to nodes
-  // compute nodal velocity
-
-  // body force
-  // traction force
-  // internal force
-
-  // solve equation of motion
-
-  // update particles (stress , velocity, position)
-
 };  // MPMExplicit class
 }  // namespace mpm
 
