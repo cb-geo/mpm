4dc4ea0278314f595e708a99625386e82989350c
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2018-06-15 12:58:41 +0100

    :dart: Add unit test to check analysis JSON object

--- tests/io_test.cc
+++ tests/io_test.cc
@@ -44,18 +44,35 @@ TEST_CASE("IO is checked for input parsing", "[IO][JSON]") {
     //! Create an IO object
     auto io = std::make_unique<IO>(json_file);
 
-    //! Check cmake JSON object 
+    //! Check cmake JSON object
     REQUIRE(io->file_name("cmake") == "../CMakeLists.txt");
 
     //! Material file should return an empty string, as file is missing
     std::string material_file = io->file_name("material");
     REQUIRE(material_file.empty() == true);
-      
+
     //! Check if CMake and README files are present
     REQUIRE(io->check_file("../CMakeLists.txt") == true);
     REQUIRE(io->check_file("../README.md") == true);
 
-    // Check if a non-existant file is present
+    //! Check if a non-existant file is present
     REQUIRE(io->check_file("../fail.txt") == false);
+
+    //! Get analysis object
+    Json analysis = io->analysis();
+    //! Check analysis dt 
+    REQUIRE(analysis["dt"] == json_file["analysis"]["dt"]);
+    //! Check analysis number_steps 
+    REQUIRE(analysis["number_steps"] == json_file["analysis"]["number_steps"]);
+    //! Check analysis gravity
+    REQUIRE(analysis["gravity"] == json_file["analysis"]["gravity"]);
+    //! Check analysis soil_particle_spacing 
+    REQUIRE(analysis["soil_particle_spacing"] == json_file["analysis"]["soil_particle_spacing"]);
+    //! Check analysis boundary_friction
+    REQUIRE(analysis["boundary_friction"] == json_file["analysis"]["boundary_friction"]);
+    //! Check analysis gravity
+    REQUIRE(analysis["damping"] == json_file["analysis"]["damping"]);
+    //! Check analysis gravity
+    REQUIRE(analysis["newmark"] == json_file["analysis"]["newmark"]);
   }
 }
