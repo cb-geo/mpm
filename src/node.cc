2810bab9a1757c07da10a2248622571878409c3f
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2018-06-21 22:00:35 +0100

    :dart: Test mesh create nodes

--- src/node.cc
+++ src/node.cc
@@ -5,3 +5,7 @@
 static Register<mpm::NodeBase<2>, mpm::Node<2, 2, 1>, mpm::Index,
                 const Eigen::Matrix<double, 2, 1>&>
     node2d("Node2D");
+
+static Register<mpm::NodeBase<3>, mpm::Node<3, 3, 1>, mpm::Index,
+                const Eigen::Matrix<double, 3, 1>&>
+    node3d("Node3D");
