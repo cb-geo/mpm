f709bb549526afe4026e4bc210c50aa22d1c914f
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2018-07-17 15:24:27 +0100

    :computer: :hammer: Fix affine transformation for initial guess

--- src/affine_transform.cc
+++ src/affine_transform.cc
@@ -12,43 +12,43 @@ const Eigen::Matrix<double, 2, 1> mpm::TransformR2UAffine<1, 2>::Kb =
     (Eigen::Matrix<double, 2, 1>() << 1.000000f, 0.000000f).finished();
 
 // 2D
-// M=[0 1 1 0;0 0 1 1;1 1 1 1];
+// M=[-1 1 1 -1;-1 -1 1 1;1 1 1 1];
 // K2 = transpose(M) * inverse (M*transpose(M));
 // clang-format off
 template <>
 const Eigen::Matrix<double, 4, 2> mpm::TransformR2UAffine<2, 4>::KA =
     (Eigen::Matrix<double, 4, 2>() <<
-     -0.500000f, -0.500000f,
-      0.500000f, -0.500000f,
-      0.500000f,  0.500000f,
-     -0.500000f,  0.500000f).finished();
+     -0.250000f, -0.2500000f,
+      0.250000f, -0.2500000f,
+      0.250000f,  0.2500000f,
+     -0.250000f,  0.2500000f).finished();
 // clang-format on
 
 template <>
 const Eigen::Matrix<double, 4, 1> mpm::TransformR2UAffine<2, 4>::Kb =
-    (Eigen::Matrix<double, 4, 1>() << 0.750000f, 0.250000f, -0.250000f,
+    (Eigen::Matrix<double, 4, 1>() << 0.250000f, 0.250000f, 0.250000f,
      0.250000f)
         .finished();
 
 // 3D
-// M=[0 1 1 0 0 1 1 0;0 0 1 1 0 0 1 1; 0 0 0 0 1 1 1 1; 1 1 1 1 1 1 1 1];
-// K3 = transpose(M) * inverse(M*transpose(M))
+// M=[-1 1 1 -1 -1 1 1 -1;-1 -1 1 1 -1 -1 1 1; -1 -1 -1 -1 1 1 1 1; 1 1 1 1 1 1
+// 11]; K3 = transpose(M) * inverse(M*transpose(M))
 // clang-format off
 template <>
 const Eigen::Matrix<double, 8, 3> mpm::TransformR2UAffine<3, 8>::KA =
     (Eigen::Matrix<double, 8, 3>() <<
-     -0.250000f, -0.250000f, -0.250000f,
-      0.250000f, -0.250000f, -0.250000f,
-      0.250000f,  0.250000f, -0.250000f,
-     -0.250000f,  0.250000f, -0.250000f,
-     -0.250000f, -0.250000f,  0.250000f,
-      0.250000f, -0.250000f,  0.250000f,
-      0.250000f,  0.250000f,  0.250000f,
-     -0.250000f,  0.250000f,  0.250000f).finished();
+     -0.125000f, -0.125000f, -0.125000f,
+      0.125000f, -0.125000f, -0.125000f,
+      0.125000f,  0.125000f, -0.125000f,
+     -0.125000f,  0.125000f, -0.125000f,
+     -0.125000f, -0.125000f,  0.125000f,
+      0.125000f, -0.125000f,  0.125000f,
+      0.125000f,  0.125000f,  0.125000f,
+     -0.125000f,  0.125000f,  0.125000f).finished();
 // clang-format on
 
 template <>
 const Eigen::Matrix<double, 8, 1> mpm::TransformR2UAffine<3, 8>::Kb =
-    (Eigen::Matrix<double, 8, 1>() << 0.500000f, 0.250000f, 0.000000f,
-     0.250000f, 0.250000f, 0.000000f, -0.250000f, 0.000000f)
+    (Eigen::Matrix<double, 8, 1>() << 0.125000f, 0.125000f, 0.125000f,
+     0.125000f, 0.125000f, 0.125000f, 0.125000f, 0.125000f)
         .finished();
