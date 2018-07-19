47aa8691f2644891b8bffaab32291410d3dbf280
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2018-07-19 14:29:53 +0100

    :computer: Define a mesh reader type in test

--- src/read_mesh.cc
+++ src/read_mesh.cc
@@ -4,8 +4,8 @@
 
 // ReadMeshAscii
 static Register<mpm::ReadMesh<2>, mpm::ReadMeshAscii<2>> readmesh_ascii_2d(
-    "MeshAscii2D");
+    "Ascii2D");
 
 // ReadMeshAscii
 static Register<mpm::ReadMesh<3>, mpm::ReadMeshAscii<3>> readmesh_ascii_3d(
-    "MeshAscii3D");
+    "Ascii3D");
