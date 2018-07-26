2c062fe677bc1d6bd05ff249b6ad5557f2e8af27
Ezra Tjung <ezrayst@berkeley.edu>
2018-07-26 15:52:46 -0700

    :dart: bingham test 3D without strain rate

--- tests/material/bingham_test.cc
+++ tests/material/bingham_test.cc
@@ -1,10 +1,16 @@
 #include <limits>
+#include <iostream>
 
 #include "Eigen/Dense"
 #include "catch.hpp"
 #include "json.hpp"
 
 #include "material/material.h"
+// #include "cell.h"
+#include "factory.h"
+#include "hex_shapefn.h"
+#include "node.h"
+#include "shapefn.h"
 
 //! \brief Check Bingham class
 TEST_CASE("Bingham is checked in 3D", "[material][bingham][3D]") {
@@ -66,7 +72,7 @@ TEST_CASE("Bingham is checked in 3D", "[material][bingham][3D]") {
     
   }
 
-  SECTION("Bingham check stresses") {
+  SECTION("Bingham check stresses with no strain rate") {
     unsigned id = 0;
     auto material = Factory<mpm::Material<Dim>, unsigned>::instance()->create(
         "Bingham3D", std::move(id));
@@ -96,36 +102,76 @@ TEST_CASE("Bingham is checked in 3D", "[material][bingham][3D]") {
     REQUIRE(stress(4) == Approx(0.).epsilon(Tolerance));
     REQUIRE(stress(5) == Approx(0.).epsilon(Tolerance));
 
-    // Initialise strain
-    mpm::Material<Dim>::Vector6d strain;
-    strain.setZero();
-    strain(0) = 0.0010000;
-    strain(1) = 0.0005000;
-    strain(2) = 0.0005000;
-    strain(3) = 0.0000000;
-    strain(4) = 0.0000000;
-    strain(5) = 0.0000000;
-
-    // Initialise strain rate
-    mpm::Material<Dim>::Vector6d strain_rate;
-    strain_rate.setZero();
-    strain_rate(0) = 0.0000100;
-    strain_rate(1) = 0.0000050;
-    strain_rate(2) = 0.0000050;
-    strain_rate(3) = 0.0000000;
-    strain_rate(4) = 0.0000000;
-    strain_rate(5) = 0.0000000;
+    // Initialise dstrain
+    mpm::Material<Dim>::Vector6d dstrain;
+    dstrain.setZero();
+    dstrain(0) = 0.0010000;
+    dstrain(1) = 0.0005000;
+    dstrain(2) = 0.0005000;
+    dstrain(3) = 0.0000000;
+    dstrain(4) = 0.0000000;
+    dstrain(5) = 0.0000000;
 
     // Add particle
     mpm::Index pid = 0;
     Eigen::Matrix<double, Dim, 1> coords;
-    coords << 0.75, 0.75, 0.75;
+    coords << 0.5, 0.5, 0.5;
     auto particle = std::make_shared<mpm::Particle<Dim, 1>>(pid, coords);
+  
+    // Coordinates of nodes for the cell
+    mpm::Index cell_id = 0;
+    const unsigned Dim = 3;
+    const unsigned Dof = 6;
+    const unsigned Nphases = 1;
+    const unsigned Nnodes = 8;    
+
+    coords << -2, 2, -2;
+    std::shared_ptr<mpm::NodeBase<Dim>> node0 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(0, coords);
+    coords << 2, 2, -2.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node1 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(1, coords);
+    coords << 2, 2, 2.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node2 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(2, coords);
+    coords << -2, 2, 2.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node3 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(3, coords);
+    coords << -2, -2, -2;
+    std::shared_ptr<mpm::NodeBase<Dim>> node4 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(4, coords);
+    coords << 2, -2, -2;
+    std::shared_ptr<mpm::NodeBase<Dim>> node5 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(5, coords);
+    coords << 2, -2, 2;
+    std::shared_ptr<mpm::NodeBase<Dim>> node6 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(6, coords);
+    coords << -2, -2, 2;
+    std::shared_ptr<mpm::NodeBase<Dim>> node7 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(7, coords);
+
+    std::shared_ptr<mpm::ShapeFn<Dim>> shapefn =
+      Factory<mpm::ShapeFn<Dim>>::instance()->create("SFH8");
+    
+    auto cell = std::make_shared<mpm::Cell<Dim>>(cell_id, Nnodes, shapefn);
+       
+    cell->add_node(0, node0);
+    cell->add_node(1, node1);
+    cell->add_node(2, node2);
+    cell->add_node(3, node3);
+    cell->add_node(4, node4);
+    cell->add_node(5, node5);
+    cell->add_node(6, node6);
+    cell->add_node(7, node7);
+
+    particle->assign_cell(cell);
+    auto check = particle->compute_shapefn();
 
     // Reset stress
     stress.setZero();
+   
     // Compute updated stress
-    stress = material->compute_stress(stress, strain, particle.get());
+    stress = material->compute_stress(stress, dstrain, particle.get());
 
     // Check stressees
     REQUIRE(stress(0) == Approx(16666.66666667).epsilon(Tolerance));
@@ -135,71 +181,118 @@ TEST_CASE("Bingham is checked in 3D", "[material][bingham][3D]") {
     REQUIRE(stress(4) == Approx(0.000000e+00).epsilon(Tolerance));
     REQUIRE(stress(5) == Approx(0.000000e+00).epsilon(Tolerance));
 
+  }
 
+  SECTION("Bingham check stresses with some strain rate") {
+    unsigned id = 0;
+    auto material = Factory<mpm::Material<Dim>, unsigned>::instance()->create(
+        "Bingham3D", std::move(id));
+    REQUIRE(material->id() == 0);
 
-    // // Check stress with some shear strain_rate but does not reach threshold
-    // // Initialise strain
-    // strain(0) = 0.0010000;
-    // strain(1) = 0.0005000;
-    // strain(2) = 0.0005000;
-    // strain(3) = 0.0000100;
-    // strain(4) = 0.0000200;
-    // strain(5) = 0.0000300;
+    // Initialise material
+    Json jmaterial;
+    jmaterial["density"] = 1000.;
+    jmaterial["youngs_modulus"] = 1.0E+7;
+    jmaterial["poisson_ratio"] = 0.3;
+    jmaterial["tau0"] = 771.8;
+    jmaterial["mu"] = 0.0451;
+    jmaterial["strain_cutoff"] = 0.2;
+
+    material->properties(jmaterial);
+
+    // Check material status after assigning material property
+    REQUIRE(material->property_handle() == true);
+
+    // Initialise stress
+    mpm::Material<Dim>::Vector6d stress;
+    stress.setZero();
+    REQUIRE(stress(0) == Approx(0.).epsilon(Tolerance));
+    REQUIRE(stress(1) == Approx(0.).epsilon(Tolerance));
+    REQUIRE(stress(2) == Approx(0.).epsilon(Tolerance));
+    REQUIRE(stress(3) == Approx(0.).epsilon(Tolerance));
+    REQUIRE(stress(4) == Approx(0.).epsilon(Tolerance));
+    REQUIRE(stress(5) == Approx(0.).epsilon(Tolerance));
+
+    // Initialise dstrain
+    mpm::Material<Dim>::Vector6d dstrain;
+    dstrain.setZero();
+    dstrain(0) = 0.0010000;
+    dstrain(1) = 0.0005000;
+    dstrain(2) = 0.0005000;
+    dstrain(3) = 0.0000000;
+    dstrain(4) = 0.0000000;
+    dstrain(5) = 0.0000000;
+
+    // Add particle
+    mpm::Index pid = 0;
+    Eigen::Matrix<double, Dim, 1> coords;
+    coords << 0.5, 0.5, 0.5;
+    auto particle = std::make_shared<mpm::Particle<Dim, 1>>(pid, coords);
+  
+    // Coordinates of nodes for the cell
+    mpm::Index cell_id = 0;
+    const unsigned Dim = 3;
+    const unsigned Dof = 6;
+    const unsigned Nphases = 1;
+    const unsigned Nnodes = 8;    
+
+    coords << -2, 2, -2;
+    std::shared_ptr<mpm::NodeBase<Dim>> node0 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(0, coords);
+    coords << 2, 2, -2.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node1 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(1, coords);
+    coords << 2, 2, 2.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node2 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(2, coords);
+    coords << -2, 2, 2.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node3 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(3, coords);
+    coords << -2, -2, -2;
+    std::shared_ptr<mpm::NodeBase<Dim>> node4 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(4, coords);
+    coords << 2, -2, -2;
+    std::shared_ptr<mpm::NodeBase<Dim>> node5 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(5, coords);
+    coords << 2, -2, 2;
+    std::shared_ptr<mpm::NodeBase<Dim>> node6 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(6, coords);
+    coords << -2, -2, 2;
+    std::shared_ptr<mpm::NodeBase<Dim>> node7 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(7, coords);
+
+    std::shared_ptr<mpm::ShapeFn<Dim>> shapefn =
+      Factory<mpm::ShapeFn<Dim>>::instance()->create("SFH8");
     
-    // // Initialise strain rate
-    // strain_rate(0) = 0.0000100;
-    // strain_rate(1) = 0.0000050;
-    // strain_rate(2) = 0.0000050;
-    // strain_rate(3) = 0.0000001;
-    // strain_rate(4) = 0.0000002;
-    // strain_rate(5) = 0.0000003;
+    auto cell = std::make_shared<mpm::Cell<Dim>>(cell_id, Nnodes, shapefn);
+       
+    cell->add_node(0, node0);
+    cell->add_node(1, node1);
+    cell->add_node(2, node2);
+    cell->add_node(3, node3);
+    cell->add_node(4, node4);
+    cell->add_node(5, node5);
+    cell->add_node(6, node6);
+    cell->add_node(7, node7);
 
-    // // Reset stress
-    // stress.setZero();
+    particle->assign_cell(cell);
+    auto check = particle->compute_shapefn();
 
-    // // Compute updated stress
-    // stress = material->compute_stress(stress, strain, particle.get());
+    // Yet to implement: add velocity constraint to have certain strain_rate
 
-    // // Check stressees
-    // REQUIRE(stress(0) == Approx(16666.66666666667).epsilon(Tolerance));
-    // REQUIRE(stress(1) == Approx(16666.66666666667).epsilon(Tolerance));
-    // REQUIRE(stress(2) == Approx(16666.66666666667).epsilon(Tolerance));
-    // REQUIRE(stress(3) == Approx(0.000000e+00).epsilon(Tolerance));
-    // REQUIRE(stress(4) == Approx(0.000000e+00).epsilon(Tolerance));
-    // REQUIRE(stress(5) == Approx(0.000000e+00).epsilon(Tolerance));
+    // Reset stress
+    stress.setZero();
+   
+    // Compute updated stress
+    stress = material->compute_stress(stress, dstrain, particle.get());
 
-
-
-    // // Check stress with some shear strain_rate that does reach threshold
-    // // Initialise strain
-    // strain(0) = 0.0010000;
-    // strain(1) = 0.0005000;
-    // strain(2) = 0.0005000;
-    // strain(3) = 0.0000100;
-    // strain(4) = 0.0000200;
-    // strain(5) = 0.0000300;
-    
-    // // Initialise strain rate
-    // strain_rate(0) = 1.000000;
-    // strain_rate(1) = 0.500000;
-    // strain_rate(2) = 0.500000;
-    // strain_rate(3) = 0.010000;
-    // strain_rate(4) = 0.020000;
-    // strain_rate(5) = 0.030000;
-
-    // // Reset stress
-    // stress.setZero();
-
-    // // Compute updated stress
-    // stress = material->compute_stress(stress, strain, particle.get());
-
-    // // Check stressees
-    // REQUIRE(stress(0) == Approx(16666.66666666667).epsilon(Tolerance));
-    // REQUIRE(stress(1) == Approx(16666.66666666667).epsilon(Tolerance));
-    // REQUIRE(stress(2) == Approx(16666.66666666667).epsilon(Tolerance));
-    // REQUIRE(stress(3) == Approx(8.908724740775902).epsilon(Tolerance));
-    // REQUIRE(stress(4) == Approx(17.81744948155181).epsilon(Tolerance));
-    // REQUIRE(stress(5) == Approx(26.72617422232771).epsilon(Tolerance));
+    // Check stressees
+    REQUIRE(stress(0) == Approx(16666.66666667).epsilon(Tolerance));
+    REQUIRE(stress(1) == Approx(16666.66666667).epsilon(Tolerance));
+    REQUIRE(stress(2) == Approx(16666.66666667).epsilon(Tolerance));
+    REQUIRE(stress(3) == Approx(0.000000e+00).epsilon(Tolerance));
+    REQUIRE(stress(4) == Approx(0.000000e+00).epsilon(Tolerance));
+    REQUIRE(stress(5) == Approx(0.000000e+00).epsilon(Tolerance));
 
   }
 }
