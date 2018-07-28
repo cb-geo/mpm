37483c997c755bc994b641368b889c5065fc1397
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2018-07-28 08:24:46 +0100

    :hammer: Refactor class functions with template specialisation

--- src/cell.cc
+++ src/cell.cc
@@ -2,6 +2,11 @@
 #include "factory.h"
 #include "shapefn.h"
 
+// Cell1D
+static Register<mpm::Cell<1>, mpm::Cell<1>, mpm::Index, unsigned,
+                const std::shared_ptr<mpm::ShapeFn<1>>&>
+    cell1d("C1D");
+
 // Cell2D
 static Register<mpm::Cell<2>, mpm::Cell<2>, mpm::Index, unsigned,
                 const std::shared_ptr<mpm::ShapeFn<2>>&>
