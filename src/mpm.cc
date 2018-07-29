8e5fa3f6d10d142baa16096f49b93feba665b1f1
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2018-07-29 09:52:23 +0100

    :computer: :hammer: Refactor main to remove switch dimensions

--- src/mpm.cc
+++ src/mpm.cc
@@ -5,10 +5,10 @@
 #include "mpm.h"
 #include "mpm_explicit.h"
 
-// 2D MPM
-static Register<mpm::MPM<2>, mpm::MPMExplicit<2>, std::unique_ptr<mpm::IO>&&>
+// 2D Explicit MPM
+static Register<mpm::MPM, mpm::MPMExplicit<2>, std::unique_ptr<mpm::IO>&&>
     mpm_explicit_2d("MPMExplicit2D");
 
-// MPMExplicit3D (3 DoF, 1 Phase)
-static Register<mpm::MPM<3>, mpm::MPMExplicit<3>, std::unique_ptr<mpm::IO>&&>
+// 3D Explicit MPM
+static Register<mpm::MPM, mpm::MPMExplicit<3>, std::unique_ptr<mpm::IO>&&>
     mpm_explicit_3d("MPMExplicit3D");
