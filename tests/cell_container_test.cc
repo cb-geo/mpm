b893fb839f1bad8f83c0304ab06f4a0c4ec65fed
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2018-08-05 15:38:42 +0100

    :dart: Test for_each container in cell 3D

--- tests/cell_container_test.cc
+++ tests/cell_container_test.cc
@@ -141,7 +141,7 @@ TEST_CASE("Cell container is checked for 2D case", "[cellcontainer][2D]") {
     REQUIRE(cell2->add_node(3, node2) == true);
     REQUIRE(cell2->nnodes() == 4);
 
-    // Check if update has gone through
+    // Check if cell has been initialised
     for (auto itr = cellcontainer->cbegin(); itr != cellcontainer->cend();
          ++itr)
       REQUIRE((*itr)->is_initialised() == false);
@@ -161,6 +161,10 @@ TEST_CASE("Cell container is checked for 2D case", "[cellcontainer][2D]") {
 TEST_CASE("Cell container is checked for 3D case", "[cellcontainer][3D]") {
   // Dimension
   const unsigned Dim = 3;
+  // Degrees of freedom
+  const unsigned Dof = 6;
+  // Number of phases
+  const unsigned Nphases = 1;
   // Number of nodes per cell
   const unsigned Nnodes = 8;
 
@@ -244,19 +248,91 @@ TEST_CASE("Cell container is checked for 3D case", "[cellcontainer][3D]") {
       REQUIRE((*itr)->nfunctions() == 8);
     }
 
-    /*
-    // Iterate through cell container to initialise
-    std::shared_ptr<mpm::ShapeFn<Dim>> hexsf =
-        std::make_shared<mpm::HexahedronShapeFn<Dim, 20>>();
+    // Coordinates
+    Eigen::Vector3d coords;
 
+    coords << 0, 0, 0;
+    std::shared_ptr<mpm::NodeBase<Dim>> node0 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(0, coords);
+
+    coords << 2, 0, 0;
+    std::shared_ptr<mpm::NodeBase<Dim>> node1 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(1, coords);
+
+    coords << 2, 2, 0;
+    std::shared_ptr<mpm::NodeBase<Dim>> node2 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(2, coords);
+
+    coords << 0, 2, 0;
+    std::shared_ptr<mpm::NodeBase<Dim>> node3 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(3, coords);
+
+    coords << 0, 0, 2;
+    std::shared_ptr<mpm::NodeBase<Dim>> node4 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(4, coords);
+
+    coords << 2, 0, 2;
+    std::shared_ptr<mpm::NodeBase<Dim>> node5 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(5, coords);
+
+    coords << 2, 2, 2;
+    std::shared_ptr<mpm::NodeBase<Dim>> node6 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(6, coords);
+
+    coords << 0, 2, 2;
+    std::shared_ptr<mpm::NodeBase<Dim>> node7 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(7, coords);
+
+    coords << 4, 0, 0.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node8 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(8, coords);
+
+    coords << 4, 2., 0;
+    std::shared_ptr<mpm::NodeBase<Dim>> node9 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(9, coords);
+
+    coords << 4., 0., 2.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node10 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(10, coords);
+
+    coords << 4., 2., 0.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node11 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(11, coords);
+
+    // Cell 1
+    REQUIRE(cell1->add_node(0, node0) == true);
+    REQUIRE(cell1->add_node(1, node1) == true);
+    REQUIRE(cell1->add_node(2, node2) == true);
+    REQUIRE(cell1->add_node(3, node3) == true);
+    REQUIRE(cell1->add_node(4, node4) == true);
+    REQUIRE(cell1->add_node(5, node5) == true);
+    REQUIRE(cell1->add_node(6, node6) == true);
+    REQUIRE(cell1->add_node(7, node7) == true);
+    REQUIRE(cell1->nnodes() == 8);
+
+    // Cell 2
+    REQUIRE(cell2->add_node(0, node1) == true);
+    REQUIRE(cell2->add_node(1, node8) == true);
+    REQUIRE(cell2->add_node(2, node9) == true);
+    REQUIRE(cell2->add_node(3, node2) == true);
+    REQUIRE(cell2->add_node(4, node5) == true);
+    REQUIRE(cell2->add_node(5, node10) == true);
+    REQUIRE(cell2->add_node(6, node11) == true);
+    REQUIRE(cell2->add_node(7, node6) == true);
+    REQUIRE(cell2->nnodes() == 8);
+
+    // Check if cell has been initialised
+    for (auto itr = cellcontainer->cbegin(); itr != cellcontainer->cend();
+         ++itr)
+      REQUIRE((*itr)->is_initialised() == false);
+
+    // Iterate through cell container to initialise
     cellcontainer->for_each(
-        std::bind(&mpm::Cell<Dim>::shapefn, std::placeholders::_1, hexsf));
+        std::bind(&mpm::Cell<Dim>::initialise, std::placeholders::_1));
 
     // Check if update has gone through
     for (auto itr = cellcontainer->cbegin(); itr != cellcontainer->cend();
-         ++itr) {
-      REQUIRE((*itr)->nfunctions() == 20);
-    }
-    */
+         ++itr)
+      REQUIRE((*itr)->is_initialised() == true);
   }
 }
