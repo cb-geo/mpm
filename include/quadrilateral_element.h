f300436bfc40bd3134d983caa0a422429290f107
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2018-08-25 08:48:34 -0500

    :computer: Add ability to extend shapefn for GIMP and CPDI

--- include/quadrilateral_element.h
+++ include/quadrilateral_element.h
@@ -82,18 +82,46 @@ class QuadrilateralElement : public Element<Tdim> {
   //! \retval shapefn Shape function of a given cell
   Eigen::VectorXd shapefn(const VectorDim& xi) const override;
 
+  //! Evaluate shape functions at given local coordinates
+  //! \param[in] xi given local coordinates
+  //! \param[in] particle_size Particle size
+  //! \param[in] deformation_gradient Deformation gradient
+  //! \retval shapefn Shape function of a given cell
+  Eigen::VectorXd shapefn(const VectorDim& xi, const VectorDim& particle_size,
+                          const VectorDim& deformation_gradient) const override;
+
   //! Evaluate gradient of shape functions
   //! \param[in] xi given local coordinates
   //! \retval grad_shapefn Gradient of shape function of a given cell
   Eigen::MatrixXd grad_shapefn(const VectorDim& xi) const override;
+
+  //! Evaluate gradient of shape functions
+  //! \param[in] xi given local coordinates
+  //! \param[in] particle_size Particle size
+  //! \param[in] deformation_gradient Deformation gradient
+  //! \retval grad_shapefn Gradient of shape function of a given cell
+  Eigen::MatrixXd grad_shapefn(
+      const VectorDim& xi, const VectorDim& particle_size,
+      const VectorDim& deformation_gradient) const override;
 
   //! Compute Jacobian
   //! \param[in] xi given local coordinates
   //! \param[in] nodal_coordinates Coordinates of nodes forming the cell
   //! \retval jacobian Jacobian matrix
   Eigen::Matrix<double, Tdim, Tdim> jacobian(
-      const Eigen::Matrix<double, 2, 1>& xi,
+      const VectorDim& xi,
       const Eigen::MatrixXd& nodal_coordinates) const override;
+
+  //! Compute Jacobian
+  //! \param[in] xi given local coordinates
+  //! \param[in] nodal_coordinates Coordinates of nodes forming the cell
+  //! \param[in] particle_size Particle size
+  //! \param[in] deformation_gradient Deformation gradient
+  //! \retval jacobian Jacobian matrix
+  Eigen::Matrix<double, Tdim, Tdim> jacobian(
+      const VectorDim& xi, const Eigen::MatrixXd& nodal_coordinates,
+      const VectorDim& particle_size,
+      const VectorDim& deformation_gradient) const override;
 
   //! Evaluate the B matrix at given local coordinates
   //! \param[in] xi given local coordinates
@@ -107,6 +135,17 @@ class QuadrilateralElement : public Element<Tdim> {
   std::vector<Eigen::MatrixXd> bmatrix(
       const VectorDim& xi,
       const Eigen::MatrixXd& nodal_coordinates) const override;
+
+  //! Evaluate the B matrix at given local coordinates for a real cell
+  //! \param[in] xi given local coordinates
+  //! \param[in] nodal_coordinates Coordinates of nodes forming the cell
+  //! \param[in] particle_size Particle size
+  //! \param[in] deformation_gradient Deformation gradient
+  //! \retval bmatrix B matrix
+  std::vector<Eigen::MatrixXd> bmatrix(
+      const VectorDim& xi, const Eigen::MatrixXd& nodal_coordinates,
+      const VectorDim& particle_size,
+      const VectorDim& deformation_gradient) const override;
 
   //! Evaluate the mass matrix
   //! \param[in] xi_s Vector of local coordinates
