c21d6062702a0031913d7156fb66999c6f6d72a9
Ezra Tjung <ezrayst@berkeley.edu>
2018-09-12 00:05:49 -0700

    :art: add compute euler_angle function

--- include/geometry.h
+++ include/geometry.h
@@ -15,8 +15,8 @@ class Geometry {
   Geometry() = default;
 
   //! Compute the inverse Euler rotation matrix for an orthogonal axis
-  //! coordinate system \param[in] angles Rotation angles depending on the
-  //! dimension
+  //! coordinate system
+  //! \param[in] angles Rotation angles depending on the dimension
   Eigen::Matrix<double, Tdim, Tdim> inverse_rotation_matrix(
       const Eigen::Matrix<double, Tdim, 1>& angles) const;
 
@@ -26,6 +26,13 @@ class Geometry {
   const double angle_between_vectors(
       const Eigen::Matrix<double, Tdim, 1>& vector_a,
       const Eigen::Matrix<double, Tdim, 1>& vector_b);
+
+  //! Compute euler angles as shown in documentation
+  //! \param[in] new_axes New orthogonal coordinate systems (2 vectors for 2D, 3
+  //! vectors for 3D) \retval euler_angles Euler Angles (2 angles for 2D, 3
+  //! angles for 3D)
+  Eigen::Matrix<double, Tdim, 1> euler_angles(
+      const Eigen::Matrix<double, Tdim, Tdim>& new_axes);
 };
 }  // namespace mpm
 
