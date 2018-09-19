4bb34a732292b04763971c13961bb91a3985f49d
cw646 <cw646@cam.ac.uk>
2018-09-19 15:33:56 +0100

    :art: refactor local node for GIMP element

--- include/element.h
+++ include/element.h
@@ -140,7 +140,7 @@ class Element {
   //! \retval indices Indices that make the face
   virtual Eigen::VectorXi face_indices(unsigned face_id) const = 0;
 
-  //! Return unit element volume for particle size calculation
+  //! Return unit element volume
   virtual double unit_element_volume() const = 0;
 };
 
