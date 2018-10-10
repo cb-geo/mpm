aaeb2f82502d7e81af4c9716cc2601e5493046a9
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2018-10-10 09:20:43 +0100

    :fire: :computer: Require particle handle for all materials

--- include/material/material.h
+++ include/material/material.h
@@ -71,13 +71,6 @@ class Material {
   //! Compute stress
   //! \param[in] stress Stress
   //! \param[in] dstrain Strain
-  //! \retval updated_stress Updated value of stress
-  virtual Vector6d compute_stress(const Vector6d& stress,
-                                  const Vector6d& dstrain) = 0;
-
-  //! Compute stress
-  //! \param[in] stress Stress
-  //! \param[in] dstrain Strain
   //! \param[in] particle Constant point to particle base
   //! \retval updated_stress Updated value of stress
   virtual Vector6d compute_stress(const Vector6d& stress,
