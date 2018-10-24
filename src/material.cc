178c4043ebe152f9c50cb074c55f15257980211c
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2018-10-24 10:58:26 +0100

    :computer: Newtonian material

--- src/material.cc
+++ src/material.cc
@@ -1,6 +1,7 @@
 #include "material/material.h"
 #include "material/bingham.h"
 #include "material/linear_elastic.h"
+#include "material/newtonian.h"
 
 // Bingham 2D
 static Register<mpm::Material<2>, mpm::Bingham<2>, unsigned, const Json&>
@@ -17,3 +18,11 @@ static Register<mpm::Material<2>, mpm::LinearElastic<2>, unsigned, const Json&>
 // LinearElastic 3D
 static Register<mpm::Material<3>, mpm::LinearElastic<3>, unsigned, const Json&>
     linear_elastic_3d("LinearElastic3D");
+
+// Newtonian 2D
+static Register<mpm::Material<2>, mpm::Newtonian<2>, unsigned, const Json&>
+    newtonian_2d("Newtonian2D");
+
+// Newtonian 3D
+static Register<mpm::Material<3>, mpm::Newtonian<3>, unsigned, const Json&>
+    newtonian_3d("Newtonian3D");
