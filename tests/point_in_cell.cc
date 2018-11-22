442dc89695edf88732e7eb9045b88d9fa72af064
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2018-11-22 19:08:46 +0000

    :construction: Use Norms to determine NR or affine for final output

--- tests/point_in_cell.cc
+++ tests/point_in_cell.cc
@@ -25,6 +25,7 @@ TEST_CASE("Point in cell 2D", "[PointInCell][2D]") {
   // Tolerance
   const double Tolerance = 1.E-7;
 
+  /*
   SECTION("Transform real to unit cell analytical solution") {
     // Number of nodes in cell
     const unsigned Nnodes = 4;
@@ -119,5 +120,65 @@ TEST_CASE("Point in cell 2D", "[PointInCell][2D]") {
     point << 0.0597025, 0.534722;
     // Test if point is in cell
     REQUIRE(cell->is_point_in_cell(point) == true);
+  }
+
+  */
+
+  SECTION("Transform real to unit cell analytical solution") {
+    // Number of nodes in cell
+    const unsigned Nnodes = 4;
+
+    // Coordinates
+    Eigen::Vector2d coords;
+
+    coords << 0.899835351184034, 2.41201458730691;
+    std::shared_ptr<mpm::NodeBase<Dim>> node0 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(0, coords);
+
+    coords << 0.900000000003745, 2.5;
+    std::shared_ptr<mpm::NodeBase<Dim>> node1 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(1, coords);
+
+    coords << 0.800000000003329, 2.5;
+    std::shared_ptr<mpm::NodeBase<Dim>> node2 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(2, coords);
+
+    coords << 0.804253384820319, 2.41407134328757;
+    std::shared_ptr<mpm::NodeBase<Dim>> node3 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(3, coords);
+
+    // 4-noded quadrilateral shape functions
+    std::shared_ptr<mpm::Element<Dim>> element =
+        Factory<mpm::Element<Dim>>::instance()->create("ED2Q4");
+
+    mpm::Index id = 0;
+    auto cell = std::make_shared<mpm::Cell<Dim>>(id, Nnodes, element);
+
+    REQUIRE(cell->add_node(0, node0) == true);
+    REQUIRE(cell->add_node(1, node1) == true);
+    REQUIRE(cell->add_node(2, node2) == true);
+    REQUIRE(cell->add_node(3, node3) == true);
+    REQUIRE(cell->nnodes() == 4);
+
+    // Initialise cell
+    REQUIRE(cell->initialise() == true);
+
+    // Coordinates of a point in real cell
+    Eigen::Vector2d point;
+    point << 0.879474, 2.43095;
+    // Test if point is in cell
+    REQUIRE(cell->is_point_in_cell(point) == true);
+
+    point << 0.87903, 2.4815;
+    // Test if point is in cell
+    // REQUIRE(cell->is_point_in_cell(point) == true);
+
+    point << 0.821834, 2.48175;
+    // Test if point is in cell
+    // REQUIRE(cell->is_point_in_cell(point) == true);
+
+    point << 0.823751, 2.43189;
+    // Test if point is in cell
+    // REQUIRE(cell->is_point_in_cell(point) == true);
   }
 }
