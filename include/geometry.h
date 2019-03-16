ce4499fb5b12989f98d2f62fc75225739255e8cb
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2019-03-15 23:57:57 -0500

    :art: Restructure geometry functions

--- include/geometry.h
+++ include/geometry.h
@@ -4,10 +4,8 @@
 #include "Eigen/Dense"
 
 namespace mpm {
-//! geometric manipulations / operations
 namespace geometry {
-//! Compute the Euler rotation matrix for an orthogonal axis
-//! coordinate system
+//! Compute the Euler rotation matrix in orthogonal axis coordinate system
 //! \param[in] angles Rotation angles depending on the dimension
 //! \tparam Tdim Dimension
 template <int Tdim>
@@ -19,50 +17,20 @@ Eigen::Matrix<double, Tdim, Tdim> rotation_matrix(
 //! \param[in] vector_b Second vector
 //! \tparam Tdim Dimension
 template <int Tdim>
-double angle_between_vectors(const Eigen::Matrix<double, Tdim, 1>& vector_a,
-                             const Eigen::Matrix<double, Tdim, 1>& vector_b) {
-  // angle between two vectors a and b = arccos( a dot b / ||a|| ||b||)
-  return acos((vector_a.normalized()).dot((vector_b.normalized())));
-}
+inline double angle_between_vectors(
+    const Eigen::Matrix<double, Tdim, 1>& vector_a,
+    const Eigen::Matrix<double, Tdim, 1>& vector_b);
 
-//! Compute euler angles with respect to the Cartesian coordinates
-//! \param[in] new_axes New orthogonal coordinate systems (2 vectors for 2D, 3
-//! vectors for 3D)
-//! \retval euler_angles Euler Angles (2 angles for 2D, 3
-//! angles for 3D)
+//! Compute Euler angles with respect to the Cartesian coordinates
+//! \param[in] new_axes New orthogonal coordinate systems
+//! \retval euler_angles Euler Angles (2 for 2D, 3 for 3D)
 //! \tparam Tdim Dimension
 template <int Tdim>
-Eigen::Matrix<double, Tdim, 1> euler_angles_cartesian(
-    const Eigen::Matrix<double, Tdim, Tdim>& new_axes) {
-
-  // Make cartesian coordinate system
-  Eigen::Matrix<double, Tdim, Tdim> original_axes;
-  original_axes.setIdentity();
-
-  // Compute line of nodes vector that bisects original x and y unit vector
-  Eigen::Matrix<double, Tdim, 1> line_of_nodes =
-      original_axes.col(0) + original_axes.col(1);
-  line_of_nodes = line_of_nodes.normalized();
-
-  // Make a vector of euler angles
-  Eigen::Matrix<double, Tdim, 1> euler_angles;
-
-  // Compute alpha
-  euler_angles(0) = acos(
-      (original_axes.col(0).normalized()).dot((line_of_nodes.normalized())));
-
-  // Compute beta
-  euler_angles(1) =
-      acos((line_of_nodes.normalized()).dot((new_axes.col(0).normalized())));
-
-  // Compute gamma
-  if (Tdim == 3)
-    euler_angles(2) = acos((new_axes.col(2).normalized())
-                               .dot((original_axes.col(2).normalized())));
-
-  return euler_angles;
-}
+inline Eigen::Matrix<double, Tdim, 1> euler_angles_cartesian(
+    const Eigen::Matrix<double, Tdim, Tdim>& new_axes);
 }  // namespace geometry
 }  // namespace mpm
+
+#include "geometry.tcc"
 
 #endif  // MPM_GEOMETRY_H_
