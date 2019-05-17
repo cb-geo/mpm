a4296c77bd7955dfe2ed6294498fbebcb55bc780
z149181511 <149181511@qq.com>
2019-05-16 19:21:07 -0700

    uwp

--- src/particle.cc
+++ src/particle.cc
@@ -11,3 +11,13 @@ static Register<mpm::ParticleBase<2>, mpm::Particle<2, 1>, mpm::Index,
 static Register<mpm::ParticleBase<3>, mpm::Particle<3, 1>, mpm::Index,
                 const Eigen::Matrix<double, 3, 1>&>
     particle3d("P3D");
+
+// Particle2D (2 Dim, 2 Phase)
+static Register<mpm::ParticleBase<2>, mpm::Particle<2, 2>, mpm::Index,
+                const Eigen::Matrix<double, 2, 1>&>
+    particle2d("P2D2P");
+
+// Particle3D (3 DoF, 2 Phase)
+static Register<mpm::ParticleBase<3>, mpm::Particle<3, 2>, mpm::Index,
+                const Eigen::Matrix<double, 3, 1>&>
+    particle3d("P3D2P");
