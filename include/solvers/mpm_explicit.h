39bb23201677ac1dc4744a36dac94425acbe2f97
shyamini <nskshyamini@gmail.com>
2019-07-15 13:11:25 -0700

    :computer: :hammer: Add solvers to a new folder

---
+++ include/solvers/mpm_explicit.h
@@ -0,0 +1,65 @@
+#ifndef MPM_MPM_EXPLICIT_H_
+#define MPM_MPM_EXPLICIT_H_
+
+#include "mpm_base.h"
+
+namespace mpm {
+
+//! MPMExplicit class
+//! \brief A class that implements the fully explicit one phase mpm
+//! \details A single-phase explicit MPM
+//! \tparam Tdim Dimension
+template <unsigned Tdim>
+class MPMExplicit : public MPMBase<Tdim> {
+ public:
+  //! Default constructor
+  MPMExplicit(std::unique_ptr<IO>&& io);
+
+  //! Solve
+  bool solve() override;
+
+ protected:
+  // Generate a unique id for the analysis
+  using mpm::MPMBase<Tdim>::uuid_;
+  //! Time step size
+  using mpm::MPMBase<Tdim>::dt_;
+  //! Current step
+  using mpm::MPMBase<Tdim>::step_;
+  //! Number of steps
+  using mpm::MPMBase<Tdim>::nsteps_;
+  //! Output steps
+  using mpm::MPMBase<Tdim>::output_steps_;
+  //! A unique ptr to IO object
+  using mpm::MPMBase<Tdim>::io_;
+  //! JSON analysis object
+  using mpm::MPMBase<Tdim>::analysis_;
+  //! JSON post-process object
+  using mpm::MPMBase<Tdim>::post_process_;
+  //! Logger
+  using mpm::MPMBase<Tdim>::console_;
+
+  //! velocity update
+  using mpm::MPMBase<Tdim>::velocity_update_;
+  //! Gravity
+  using mpm::MPMBase<Tdim>::gravity_;
+  //! Mesh object
+  using mpm::MPMBase<Tdim>::mesh_;
+  //! Materials
+  using mpm::MPMBase<Tdim>::materials_;
+  //! VTK attributes
+  using mpm::MPMBase<Tdim>::vtk_attributes_;
+  //! Bool nodal tractions
+  using mpm::MPMBase<Tdim>::nodal_tractions_;
+
+ private:
+  //! Boolean to switch between USL and USF
+  bool usl_{false};
+  //! Pressure smoothing
+  bool pressure_smoothing_{false};
+
+};  // MPMExplicit class
+}  // namespace mpm
+
+#include "mpm_explicit.tcc"
+
+#endif  // MPM_MPM_EXPLICIT_H_
