3267157ee1fd284bd1c81c2e06b3071d3805f001
shyamini <nskshyamini@gmail.com>
2019-10-08 15:43:15 -0700

    :computer: Temperory remove sine function header

--- src/function.cc
+++ src/function.cc
@@ -1,12 +1,7 @@
 #include "factory.h"
 #include "functions/function_base.h"
 #include "functions/step_function.h"
-#include "functions/sine_function.h"
 
 // Step function
 static Register<mpm::FunctionBase, mpm::StepFunction, unsigned, const Json&>
     stepfunction("STEP");
-
-// Sine function
-static Register<mpm::FunctionBase, mpm::SineFunction, unsigned, const Json&>
-    sinefunction("SINE");
