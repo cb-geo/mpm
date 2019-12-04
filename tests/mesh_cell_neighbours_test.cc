2ba3f0e2adde87a59d9e1956dcdf93c3af61b017
Nanda <bodhinandach@gmail.com>
2019-12-03 23:07:17 -0800

    test 2D completed

--- tests/mesh_cell_neighbours_test.cc
+++ tests/mesh_cell_neighbours_test.cc
@@ -61,6 +61,46 @@ TEST_CASE("Mesh cell neighbours 2D", "[MeshCell][2D]") {
     std::shared_ptr<mpm::NodeBase<Dim>> node5 =
         std::make_shared<mpm::Node<Dim, Dof, Nphases>>(5, coords);
 
+    coords << 4., 4.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node6 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(6, coords);
+
+    coords << 2., 4.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node7 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(7, coords);
+
+    coords << 0., 4.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node8 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(8, coords);
+
+    coords << 6., 0.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node9 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(9, coords);
+
+    coords << 6., 2.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node10 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(10, coords);
+
+    coords << 6., 4.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node11 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(11, coords);
+
+    coords << 6., 6.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node12 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(12, coords);
+
+    coords << 4., 6.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node13 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(13, coords);
+
+    coords << 2., 6.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node14 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(14, coords);
+
+    coords << 0., 6.;
+    std::shared_ptr<mpm::NodeBase<Dim>> node15 =
+        std::make_shared<mpm::Node<Dim, Dof, Nphases>>(15, coords);
+
     // 4-noded quadrilateral shape functions
     std::shared_ptr<mpm::Element<Dim>> element =
         Factory<mpm::Element<Dim>>::instance()->create("ED2Q4");
@@ -90,6 +130,97 @@ TEST_CASE("Mesh cell neighbours 2D", "[MeshCell][2D]") {
     // Initialise cell and add to mesh
     REQUIRE(cell1->initialise() == true);
     REQUIRE(mesh->add_cell(cell1) == true);
+
+    id = 2;
+    auto cell2 = std::make_shared<mpm::Cell<Dim>>(id, Nnodes, element);
+
+    REQUIRE(cell2->add_node(0, node2) == true);
+    REQUIRE(cell2->add_node(1, node5) == true);
+    REQUIRE(cell2->add_node(2, node6) == true);
+    REQUIRE(cell2->add_node(3, node7) == true);
+    REQUIRE(cell2->nnodes() == 4);
+
+    // Initialise cell and add to mesh
+    REQUIRE(cell2->initialise() == true);
+    REQUIRE(mesh->add_cell(cell2) == true);
+
+    id = 3;
+    auto cell3 = std::make_shared<mpm::Cell<Dim>>(id, Nnodes, element);
+
+    REQUIRE(cell3->add_node(0, node3) == true);
+    REQUIRE(cell3->add_node(1, node2) == true);
+    REQUIRE(cell3->add_node(2, node7) == true);
+    REQUIRE(cell3->add_node(3, node8) == true);
+    REQUIRE(cell3->nnodes() == 4);
+
+    // Initialise cell and add to mesh
+    REQUIRE(cell3->initialise() == true);
+    REQUIRE(mesh->add_cell(cell3) == true);
+
+    id = 4;
+    auto cell4 = std::make_shared<mpm::Cell<Dim>>(id, Nnodes, element);
+
+    REQUIRE(cell4->add_node(0, node4) == true);
+    REQUIRE(cell4->add_node(1, node9) == true);
+    REQUIRE(cell4->add_node(2, node10) == true);
+    REQUIRE(cell4->add_node(3, node5) == true);
+    REQUIRE(cell4->nnodes() == 4);
+
+    // Initialise cell and add to mesh
+    REQUIRE(cell4->initialise() == true);
+    REQUIRE(mesh->add_cell(cell4) == true);
+
+    id = 5;
+    auto cell5 = std::make_shared<mpm::Cell<Dim>>(id, Nnodes, element);
+
+    REQUIRE(cell5->add_node(0, node5) == true);
+    REQUIRE(cell5->add_node(1, node10) == true);
+    REQUIRE(cell5->add_node(2, node11) == true);
+    REQUIRE(cell5->add_node(3, node6) == true);
+    REQUIRE(cell5->nnodes() == 4);
+
+    // Initialise cell and add to mesh
+    REQUIRE(cell5->initialise() == true);
+    REQUIRE(mesh->add_cell(cell5) == true);
+
+    id = 6;
+    auto cell6 = std::make_shared<mpm::Cell<Dim>>(id, Nnodes, element);
+
+    REQUIRE(cell6->add_node(0, node6) == true);
+    REQUIRE(cell6->add_node(1, node11) == true);
+    REQUIRE(cell6->add_node(2, node12) == true);
+    REQUIRE(cell6->add_node(3, node13) == true);
+    REQUIRE(cell6->nnodes() == 4);
+
+    // Initialise cell and add to mesh
+    REQUIRE(cell6->initialise() == true);
+    REQUIRE(mesh->add_cell(cell6) == true);
+
+    id = 7;
+    auto cell7 = std::make_shared<mpm::Cell<Dim>>(id, Nnodes, element);
+
+    REQUIRE(cell7->add_node(0, node7) == true);
+    REQUIRE(cell7->add_node(1, node6) == true);
+    REQUIRE(cell7->add_node(2, node13) == true);
+    REQUIRE(cell7->add_node(3, node14) == true);
+    REQUIRE(cell7->nnodes() == 4);
+
+    // Initialise cell and add to mesh
+    REQUIRE(cell7->initialise() == true);
+    REQUIRE(mesh->add_cell(cell7) == true);
+
+    id = 8;
+    auto cell8 = std::make_shared<mpm::Cell<Dim>>(id, Nnodes, element);
+
+    REQUIRE(cell8->add_node(0, node8) == true);
+    REQUIRE(cell8->add_node(1, node7) == true);
+    REQUIRE(cell8->add_node(2, node14) == true);
+    REQUIRE(cell8->add_node(3, node15) == true);
+    REQUIRE(cell8->nnodes() == 4);
+
+    // Initialise cell and add to mesh
+    REQUIRE(cell8->initialise() == true);
+    REQUIRE(mesh->add_cell(cell8) == true);
 
     SECTION("Test face node neighbour ids") {
       // Check face node ids of cell 0
@@ -133,10 +264,36 @@ TEST_CASE("Mesh cell neighbours 2D", "[MeshCell][2D]") {
 
     SECTION("Compute cell neighbours") {
       mesh->compute_cell_neighbours();
-      REQUIRE(cell0->nneighbours() == 1);
-      REQUIRE(cell1->nneighbours() == 1);
-      for (auto n : cell0->neighbours()) REQUIRE(n == 1);
-      for (auto n : cell1->neighbours()) REQUIRE(n == 0);
+      REQUIRE(cell0->nneighbours() == 3);
+      REQUIRE(cell1->nneighbours() == 5);
+      REQUIRE(cell2->nneighbours() == 8);
+      REQUIRE(cell3->nneighbours() == 5);
+      REQUIRE(cell4->nneighbours() == 3);
+      REQUIRE(cell5->nneighbours() == 5);
+      REQUIRE(cell6->nneighbours() == 3);
+      REQUIRE(cell7->nneighbours() == 5);
+      REQUIRE(cell8->nneighbours() == 3);
+
+      // Check solutions
+      std::set<mpm::Index> n0 = {1, 2, 3};
+      std::set<mpm::Index> n1 = {0, 2, 3, 4, 5};
+      std::set<mpm::Index> n2 = {0, 1, 3, 4, 5, 6, 7, 8};
+      std::set<mpm::Index> n3 = {0, 1, 2, 7, 8};
+      std::set<mpm::Index> n4 = {1, 2, 5};
+      std::set<mpm::Index> n5 = {1, 2, 4, 6, 7};
+      std::set<mpm::Index> n6 = {2, 5, 7};
+      std::set<mpm::Index> n7 = {2, 3, 5, 6, 8};
+      std::set<mpm::Index> n8 = {2, 3, 7};
+
+      REQUIRE(cell0->neighbours() == n0);
+      REQUIRE(cell1->neighbours() == n1);
+      REQUIRE(cell2->neighbours() == n2);
+      REQUIRE(cell3->neighbours() == n3);
+      REQUIRE(cell4->neighbours() == n4);
+      REQUIRE(cell5->neighbours() == n5);
+      REQUIRE(cell6->neighbours() == n6);
+      REQUIRE(cell7->neighbours() == n7);
+      REQUIRE(cell8->neighbours() == n8);
 
       SECTION("Locate particles in mesh") {
         coords << 3., 1.5;
