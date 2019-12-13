5ea45b7a98a2392fa890a627758522abcdf305cb
shyamini <nskshyamini@gmail.com>
2019-12-13 10:32:47 -0800

    :hammer: Refactor two phase particle

--- src/particle.cc
+++ src/particle.cc
@@ -1,23 +1,24 @@
 #include "particle.h"
+#include "twophase_particle.h"
 #include "factory.h"
 #include "particle_base.h"
 
-// Particle2D (2 Dim)
+// Single phase particle2D (2 Dim)
 static Register<mpm::ParticleBase<2>, mpm::Particle<2>, mpm::Index,
                 const Eigen::Matrix<double, 2, 1>&>
     particle2d("P2D");
 
-// Particle3D (3 Dim)
+// Single phase particle3D (3 Dim)
 static Register<mpm::ParticleBase<3>, mpm::Particle<3>, mpm::Index,
                 const Eigen::Matrix<double, 3, 1>&>
     particle3d("P3D");
 
-// Particle2D (2 Dim, 2 Phase)
-static Register<mpm::ParticleBase<2>, mpm::Particle<2, 2>, mpm::Index,
+// Second phase particle2D (2 Dim)
+static Register<mpm::ParticleBase<2>, mpm::TwoPhaseParticle<2>, mpm::Index,
                 const Eigen::Matrix<double, 2, 1>&>
     particle2d2phase("P2D2PHASE");
 
-// Particle3D (3 DoF, 2 Phase)
-static Register<mpm::ParticleBase<3>, mpm::Particle<3, 2>, mpm::Index,
+// Second phase particle3D (3 Dim)
+static Register<mpm::ParticleBase<3>, mpm::TwoPhaseParticle<3>, mpm::Index,
                 const Eigen::Matrix<double, 3, 1>&>
     particle3d2phase("P3D2PHASE");
