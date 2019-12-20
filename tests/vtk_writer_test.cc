1aa3a42e996810da3c171505fa63acbf8aa766e2
Krishna Kumar <krishnak@utexas.edu>
2019-12-20 10:41:13 -0600

    :dart: Check file contents

--- tests/vtk_writer_test.cc
+++ tests/vtk_writer_test.cc
@@ -20,6 +20,37 @@ TEST_CASE("VTK Writer is checked", "[vtk][writer]") {
     vtk_writer->write_parallel_vtk(parallel_vtk_file, attribute, mpi_size, step,
                                    max_steps);
 
+    // Check file data
+    std::string ppolydata =
+        "<?xml version=\"1.0\"?>\n<VTKFile type=\"PPolyData\" version=\"0.1\" "
+        "byte_order=\"LittleEndian\" "
+        "compressor=\"vtkZLibDataCompressor\">\n<PPolyData "
+        "GhostLevel=\"0\">\n\t<PPointData Vectors=\"" +
+        attribute +
+        "\">\n\t\t<PDataArray "
+        "type=\"Float64\" Name=\"" +
+        attribute +
+        "\" "
+        "NumberOfComponents=\"3\"/>\n\t</"
+        "PPointData>\n\n\t<PPoints>\n\t\t<PDataArray "
+        "type=\"Float32\" Name=\"Points\" "
+        "NumberOfComponents=\"3\"/>\n\t</PPoints>\n";
+
+    for (unsigned i = 0; i < mpi_size; ++i)
+      ppolydata += "\n\t<Piece Source=\"stress-" + std::to_string(i) + "_" +
+                   std::to_string(mpi_size) + "-01000.vtp\"/>";
+
+    ppolydata += "\n</PPolyData>\n\n</VTKFile>";
+
+    // Check if file exists
     REQUIRE(boost::filesystem::exists(parallel_vtk_file) == true);
+
+    std::ifstream ifs(parallel_vtk_file);
+    std::string content((std::istreambuf_iterator<char>(ifs)),
+                        (std::istreambuf_iterator<char>()));
+
+    // Check file content
+    REQUIRE(content.length() == ppolydata.length());
+    REQUIRE(content == ppolydata);
   }
 }
