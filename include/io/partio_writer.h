477fd9841723af6b9b184a242bca9b4784312169
Krishna Kumar <krishnak@utexas.edu>
2020-01-16 11:21:06 -0600

    :computer: :art: Write Partio writer

---
+++ include/io/partio_writer.h
@@ -0,0 +1,23 @@
+#ifndef PARTIO_WRITER_H_
+#define PARTIO_WRITER_H_
+
+#ifdef USE_PARTIO
+#include <vector>
+
+#include <Partio.h>
+
+#include "data_types.h"
+#include "hdf5_particle.h"
+
+namespace mpm::partio {
+
+//! Write mesh
+//! \param[in] filename Mesh VTP file
+//! \param[in] particles HDF5 particles
+bool write_partio(const std::string& filename,
+                  const std::vector<mpm::HDF5Particle>& particles);
+
+}  // namespace mpm::partio
+
+#endif  // USE_PARTIO
+#endif  // PARTIO_WRITER_H_
