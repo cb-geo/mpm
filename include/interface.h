e29ee3bfb9acd457a788bb5e4111a23f56f59bdf
Thiago Araujo <thiago.araujo@utexas.edu>
2020-02-18 14:41:27 -0600

    :computer: :construction: Create an Interface with parameters from interface/contact law

---
+++ include/interface.h
@@ -0,0 +1,55 @@
+#ifndef MPM_INTERFACE_INTERFACE_H_
+#define MPM_INTERFACE_INTERFACE_H_
+
+#include <limits>
+#include <set>
+
+#include "Eigen/Dense"
+#include "json.hpp"
+
+#include "logger.h"
+
+// JSON
+using Json = nlohmann::json;
+
+namespace mpm {
+
+//! Interface base class
+//! \brief Interface class that stores the information about interface law
+//! \details Interface class with interface law
+class Interface {
+ public:
+  // Constructor with id
+  //! \param[in] id Interface id
+  Interface(unsigned id, const Json& interface_properties);
+
+  //! Destructor
+  virtual ~Interface(){};
+
+  //! Delete copy constructor
+  Interface(const Interface&) = delete;
+
+  //! Delete assignement operator
+  Interface& operator=(const Interface&) = delete;
+
+  //! Return id of the interface
+  unsigned id() const { return id_; }
+
+  //! Return friction coefficient of the interface
+  double friction() const { return friction_; }
+
+ private:
+  //! interface id
+  unsigned id_{std::numeric_limits<unsigned>::max()};
+  //! Friction coefficient
+  double friction_{0};
+  //! Set of materials that share this interface law
+  std::set<unsigned> materials_;
+  //! Logger
+  std::unique_ptr<spdlog::logger> console_;
+};  // Interface class
+}  // namespace mpm
+
+#include "interface.tcc"
+
+#endif  // MPM_INTERFACE_INTERFACE_H_
---
+++ include/interface.tcc
@@ -0,0 +1,20 @@
+//! Read interface properties
+mpm::Interface::Interface(unsigned id, const Json& interface_properties)
+    : id_{id} {
+  //! Logger
+  std::string logger = "interface::" + std::to_string(id);
+  console_ = std::make_unique<spdlog::logger>(logger, mpm::stdout_sink);
+
+  //! Read interface properties
+  try {
+    // Read Friction
+    friction_ = interface_properties.at("friction").template get<double>();
+
+    // Read set of nodes
+    std::set<unsigned> materials_ = interface_properties.at("material_ids");
+
+  } catch (Json::exception& except) {
+    console_->error("Interface parameter not set: {} {}\n", except.what(),
+                    except.id);
+  }
+}
