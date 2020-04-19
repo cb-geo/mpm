8553ed8a6f9707af5455149ed31eb304973d671f
Jiewen Lai <alanljw12345@gmail.com>
2020-04-19 00:36:08 -0500

    Add test skeleton

--- tests/petsc_test.cc
+++ tests/petsc_test.cc
@@ -3,22 +3,27 @@
 #include "catch.hpp"
 
 #ifdef USE_PETSC
-TEST_CASE("PETSC Solver checked", "[solver][petsc]") {
-    SECTION("PESTC Solver's results are consistent with EigenCG") {
-        // TODO: pick suitable constants
-        constexpr unsigned max_iter = 100;
-        constexpr double tolerance = 10e-5;
+TEST_CASE("PETSC solver checked", "[solver][petsc]") {
+    // TODO: pick suitable constants
+    constexpr unsigned max_iter = 100;
+    constexpr double tolerance = 10e-5;
+    Eigen::SparseMatrix<double> A;
+    Eigen::VectorXd b;
+    SECTION("PESTC solver's results are checked") {
         auto petsc_matrix_solver =
             Factory<mpm::SolverBase<Eigen::SparseMatrix<double>>, unsigned, double>::instance()
                 ->create("KrylovPETSC", std::move(max_iter), std::move(tolerance));
-        auto reference_matrix_solver
-        Factory<mpm::SolverBase<Eigen::SparseMatrix<double>>, unsigned, double>::instance()
-            ->create("EigenCG", std::move(max_iter), std::move(tolerance));
-        Eigen::SparseMatrix<double> A;
-        Eigen::VectorXd b;
-        auto &c_petsc = petsc_matrix_solver->solve(A, b, "cg");
-        auto &c_ref = reference_matrix_solver->solve(A, b, "cg");
-        //TODO: compare result of c_petsc & c_ref
+        auto &x_petsc = petsc_matrix_solver->solve(A, b, "cg");
+        bool pass_compare = true;
+        REQUIRE(pass_compare);
+    }
+
+    SECTION("Eigen solver's results are checked") {
+
+        auto eigen_matrix_solver =
+            Factory<mpm::SolverBase<Eigen::SparseMatrix<double>>, unsigned, double>::instance()
+                ->create("EigenCG", std::move(max_iter), std::move(tolerance));
+        auto &x_eigen = eigen_matrix_solver->solve(A, b, "cg");
         bool pass_compare = true;
         REQUIRE(pass_compare);
     }
