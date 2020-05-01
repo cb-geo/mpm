b38cd278a071b8c0b3c664985d42bdc50a220164
Krishna Kumar <krishnak@utexas.edu>
2020-05-01 13:48:10 -0500

    :construction: Create partition test fails

--- tests/graph_test.cc
+++ tests/graph_test.cc
@@ -1,5 +1,7 @@
 #ifdef USE_GRAPH_PARTITIONING
 
+#include <iostream>
+
 #include <functional>
 #include <limits>
 #include <memory>
@@ -488,7 +490,7 @@ TEST_CASE("Graph Partitioning in 2D", "[mpi][graph][2D]") {
     mesh->find_cell_neighbours();
 
     SECTION("Decompose mesh graph") {
-      if (mpi_rank == 4) {
+      if (mpi_size == 4) {
         // Create graph
         auto graph = std::make_shared<mpm::Graph<Dim>>(mesh->cells());
         mesh->find_nglobal_particles_cells();
@@ -741,25 +743,52 @@ TEST_CASE("Graph Partitioning in 3D", "[mpi][graph][3D]") {
     // Compute cell neighbours
     mesh->find_cell_neighbours();
 
+    coords << 1, 1, 1;
+    std::shared_ptr<mpm::ParticleBase<Dim>> particle0 =
+        std::make_shared<mpm::Particle<Dim>>(0, coords);
+
+    coords << 1, 1, 3;
+    std::shared_ptr<mpm::ParticleBase<Dim>> particle1 =
+        std::make_shared<mpm::Particle<Dim>>(1, coords);
+
+    coords << 3, 1, 3;
+    std::shared_ptr<mpm::ParticleBase<Dim>> particle2 =
+        std::make_shared<mpm::Particle<Dim>>(2, coords);
+
+    coords << 3, 1, 1;
+    std::shared_ptr<mpm::ParticleBase<Dim>> particle3 =
+        std::make_shared<mpm::Particle<Dim>>(3, coords);
+
+    mesh->add_particle(particle0);
+    mesh->add_particle(particle1);
+    mesh->add_particle(particle2);
+    mesh->add_particle(particle3);
+
+    auto missing = mesh->locate_particles_mesh();
+    REQUIRE(missing.size() == 0);
+    
+    REQUIRE(mesh->nparticles() == 4);
+
     SECTION("Decompose mesh graph") {
-      if (mpi_rank == 4) {
-        // Create graph
-        auto graph = std::make_shared<mpm::Graph<Dim>>(mesh->cells());
-        mesh->find_nglobal_particles_cells();
-        graph->construct_graph(mpi_size, mpi_rank);
+      if (mpi_size == 4) {
         // Initialize MPI
         MPI_Comm comm;
         MPI_Comm_dup(MPI_COMM_WORLD, &comm);
 
-        // Check number of ghost cells
-        REQUIRE(mesh->nghost_cells() == 0);
-        REQUIRE(mesh->nlocal_ghost_cells() == 0);
+        auto graph = std::make_shared<mpm::Graph<Dim>>(mesh->cells());
 
-        // Fast mode for graph partitioning
-        int mode = 4;
-        // Create partition
-        bool graphpartition = graph->create_partitions(&comm, mode);
+        // Find number of particles in each cell across MPI ranks
+        mesh->find_nglobal_particles_cells();
 
+        // Construct a weighted DAG
+        graph->construct_graph(mpi_size, mpi_rank);
+
+        // Graph partitioning mode
+        int mode = 4;  // FAST
+        // Create graph partition
+        bool graph_partition = graph->create_partitions(&comm, mode);
+        REQUIRE(graph_partition == true);
+        /*
         // Collect the partitions
         graph->collect_partitions(mpi_size, mpi_rank, &comm);
 
@@ -768,12 +797,32 @@ TEST_CASE("Graph Partitioning in 3D", "[mpi][graph][3D]") {
 
         // Identify shared nodes across MPI domains
         mesh->find_domain_shared_nodes();
-
         // Identify ghost boundary cells
         mesh->find_ghost_boundary_cells();
+        */
+        // for (unsigned i = 0; i < mpi_size; ++i) {
+        //   if (i == mpi_rank) {
+        //     std::cout << "MPI rank: " << i << " Cell 0 rank" << cell0->rank()
+        //               << "\n";
+        //     std::cout << "MPI rank: " << i << " Cell 1 rank" << cell1->rank()
+        //               << "\n";
+        //     std::cout << "MPI rank: " << i << " Cell 2 rank" << cell2->rank()
+        //               << "\n";
+        //     std::cout << "MPI rank: " << i << " Cell 3 rank" << cell3->rank()
+        //               << "\n";
+        //   }
+        // }
 
-        REQUIRE(mesh->nghost_cells() == 3);
-        REQUIRE(mesh->nlocal_ghost_cells() == 1);
+        // Identify ghost boundary cells
+        // mesh->find_ghost_boundary_cells();
+        // for (unsigned i = 0; i < mpi_size; ++i)
+        //   if (mpi_rank == 0)
+        //     std::cout << "MPI: " << mpi_rank << "\t cell" << mesh->ncells_rank()
+        //               << " ghost: " << mesh->nghost_cells() << "local_ghost"
+        //               << mesh->nlocal_ghost_cells() << "\n";
+
+        // REQUIRE(mesh->nghost_cells() == 3);
+        // REQUIRE(mesh->nlocal_ghost_cells() == 1);
       }
     }
   }
