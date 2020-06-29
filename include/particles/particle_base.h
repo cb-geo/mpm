526fc020b889b87f86118c6207276accb4f0b072
Thiago Araujo <thiago.araujo@utexas.edu>
2020-06-29 13:13:16 -0500

    :sparkles::construction: Create a function to map domain gradients from Particle to Node

--- include/particles/particle_base.h
+++ include/particles/particle_base.h
@@ -145,6 +145,9 @@ class ParticleBase {
   //! Map multimaterial displacements to nodes
   virtual void map_multimaterial_displacements_to_nodes() noexcept = 0;
 
+  //! Map multimaterial domain gradients to nodes
+  virtual void map_multimaterial_domain_gradients_to_nodes() noexcept = 0;
+
   //! Assign material
   virtual bool assign_material(
       const std::shared_ptr<Material<Tdim>>& material) = 0;
