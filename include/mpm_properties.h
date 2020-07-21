532aca85f5d3c078ee7554efd3fd6f14a64f74c5
Nanda <bodhinandach@gmail.com>
2020-07-21 10:19:33 -0700

    :wrench: add mpm_properties

---
+++ include/mpm_properties.h
@@ -0,0 +1,26 @@
+#ifndef MPM_PROPERTIES_H_
+#define MPM_PROPERTIES_H_
+
+namespace mpm {
+namespace properties {
+//! Scalar Properties
+enum Scalar : unsigned int {
+  Mass,
+  Volume,
+  MassDensity,
+  MassPressure,
+  Pressure
+};
+//! Vector Properties
+enum Vector : unsigned int {
+  Displacement,
+  Velocity,
+  Acceleration,
+  Momentum,
+  ExternalForce,
+  InternalForce
+};
+}  // namespace properties
+}  // namespace mpm
+
+#endif  // MPM_PROPERTIES_H_
