7fab0a43ce8070d3111baf1b33f3697a2c4bb7f0
Krishna Kumar <krishnak@utexas.edu>
2020-07-22 18:34:53 -0500

    :art: Update SpinMutex class

--- include/mutex.h
+++ include/mutex.h
@@ -3,30 +3,35 @@
 
 #include <atomic>
 
+//! Hybrid SpinMutex class
+//! \brief SpinMutex class that offers a fast locking and unlocking mechanism
 class SpinMutex {
- private:
-  std::atomic_flag _lock = ATOMIC_FLAG_INIT;
-  std::atomic<std::size_t> _spin_pred{0};
 
  public:
-  bool try_lock() { return !_lock.test_and_set(std::memory_order_acquire); }
+  //! Attempt locking
+  bool try_lock() { return !lock_.test_and_set(std::memory_order_acquire); }
 
+  //! Call lock 
   void lock() {
     std::size_t spin_count{0};
     while (!try_lock()) {
       ++spin_count;
 
-      if (spin_count < _spin_pred * 2) continue;
+      if (spin_count < spin_pred_ * 2) continue;
 
-      // REVISIT (fbrereto) : Iff sleep_for is guaranteed to block even
-      // when the duration is 0, then std::chrono::nanoseconds(0) will
-      // suffice here.
       std::this_thread::sleep_for(std::chrono::nanoseconds(1));
     }
 
-    _spin_pred += (spin_count - _spin_pred) / 8;
+    spin_pred_ += (spin_count - spin_pred_) / 8;
   }
 
-  void unlock() { _lock.clear(std::memory_order_release); }
+  //! Call unlock
+  void unlock() { lock_.clear(std::memory_order_release); }
+
+ private:
+  //! Lock variable
+  std::atomic_flag lock_ = ATOMIC_FLAG_INIT;
+  //! Spin prediction
+  std::atomic<std::size_t> spin_pred_{0};
 };
-#endif
+#endif  // _MUTEX_
